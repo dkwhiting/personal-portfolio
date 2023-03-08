cc1f1aff590d2e6c269c3a4114cd3a844c48d89b
dkwhiting <dallinkwhiting@gmail.com>
2023-03-08 13:30:46 -0700

    Fixed static asset paths for production

--- src/Sections/Project.jsx
+++ src/Sections/Project.jsx
@@ -2,15 +2,14 @@ import React from 'react'
 import { Fade } from 'react-awesome-reveal'
 
 const Project = ({ project }) => {
-
-  console.log(project.imgPath)
+  const imgUrl = new URL(`../assets/${project.imgPath}`, import.meta.url).href
   return (
     <Fade
       direction={"up"}
       triggerOnce={true}
     >
       <div className="project">
-        <img src={project.imgPath} alt={`${project.name} image`} />
+        <img src={imgUrl} alt={`${project.name} image`} />
         <div className="project-overlay">
 
         </div>
