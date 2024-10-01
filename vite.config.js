7ea8c9413affc4e101792659ebb1a826f14d462a
dallin <dallin.whiting@stgconsulting.com>
2024-09-30 21:52:29 -0600

    changed config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'esnext', // Or 'es2020' if compatibility with older browsers is needed
+  }
 })
